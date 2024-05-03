ecf6ab0bbda11e5edc6ef2a31f22cd8b7b85c454
Dikiliev <76054541+Dikiliev@users.noreply.github.com>
2024-05-03 12:14:48 +0300

    fix max width

--- estechbackend/config/urls.py
+++ estechbackend/config/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('api/products/', include('products.urls')),
 ]
 
-if settings.DEBUG:  # Only add these patterns when in debug mode
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
