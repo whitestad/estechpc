f8478c60ca83ddd22c8bb6723cc56484474e327b
Dikiliev <76054541+Dikiliev@users.noreply.github.com>
2024-08-06 03:43:41 +0300

    restart front

--- estech-frontend/src/components/common/horizontalLine/HorizontalLine.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './HorizontalLine.module.css';
-
-function HorizontalLine({dashed}){
-    return (
-        <hr className={[styles.horizontalLine, dashed && styles.dashed].join(' ')} />
-    );
-}
-
-export {HorizontalLine}
---
+++ estechfrontend/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  function handle() {
+    console.log('Hello World!');
+    console.log('Hello World!');
+    console.log('Hello World!');
+  }
+
+  return (
+    <>
+      <h1>Page</h1>
+      <div>
+        <p>1</p>
+        <p>2</p>
+      </div>
+      <div>es322ddsa</div>
+    </>
+  );
+}
+
+export default App;
