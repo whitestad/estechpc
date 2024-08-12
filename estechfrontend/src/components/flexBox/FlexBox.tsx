8f7add766b91f80a52929165928b02d1ed3b6bb6
Dikiliev <76054541+Dikiliev@users.noreply.github.com>
2024-08-12 09:13:51 +0300

    fixes

--- estechfrontend/src/components/flexBox/FlexBox.tsx
+++ estechfrontend/src/components/flexBox/FlexBox.tsx
@@ -1,35 +1,32 @@
-import React from "react";
-import { Box } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import theme from "@styles/theme";
+import React from 'react';
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
-// Создаем стилизованный компонент FlexBox, наследуя базовый компонент Box
 const FlexBoxStyled = styled(Box)(({ theme }) => ({
-    display: "flex", // Применяем свойство display: flex для создания flex-контейнера
-    flexDirection: "row", // Определяем направление элементов в контейнере (по умолчанию по горизонтали)
-    alignItems: "center", // Центрируем элементы по вертикали
-    justifyContent: "flex-start", // Выравниваем элементы в начале контейнера
-    gap: theme.spacing(2), // Определяем пространство между элементами внутри контейнера
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: theme.spacing(2),
 }));
 
-// Типы пропсов для FlexBox
 interface FlexBoxProps {
-    children: React.ReactNode; // Принимаем любые React элементы как детей
-    flexDirection?: "row" | "column"; // Добавляем возможность изменять направление элементов
-    alignItems?: "flex-start" | "center" | "flex-end" | "stretch" | "baseline";
-    justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around" | "space-evenly";
-    gap?: number | string; // Размер интервала между элементами
+    children: React.ReactNode;
+    flexDirection?: 'row' | 'column';
+    alignItems?: 'flex-start' | 'center' | 'flex-end' | 'stretch' | 'baseline';
+    justifyContent?: 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly';
+    gap?: number | string;
     bgColor?: string;
-    sx?: object; // Пропс для кастомизации стилей
+    sx?: object;
 }
 
 const FlexBox: React.FC<FlexBoxProps> = ({
     children,
-    flexDirection = "row",
-    alignItems = "center",
-    justifyContent = "flex-start",
+    flexDirection = 'row',
+    alignItems = 'center',
+    justifyContent = 'flex-start',
     gap,
-    bgColor = theme.palette.background.paper,
+    bgColor = 'transparent',
     sx,
 }) => {
     return (
--- estechfrontend/src/main.tsx
+++ estechfrontend/src/main.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from '@src/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
